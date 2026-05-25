e7ce8eeabd225a747f8a89ac697f772b60986f77
Arknncyy <michellefirstian12@gmail.com>
2026-05-25 17:59:59 +0700

    arkan coba coba

--- AplikasiUtama.java
+++ AplikasiUtama.java
@@ -2,5 +2,6 @@ public class AplikasiUtama {
     
     public static void main(String[] args) {
         System.out.println("Selamat datang di Aplikasi Utama!");
+        System.out.println("Coba coba dulu");
     }
 }
